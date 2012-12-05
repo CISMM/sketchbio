2bc9dbc6278d1f1d3fa227b787f4c997e7bcb1c1
Shawn Waldon <swaldon@cs.unc.edu>
2012-12-05 16:39:16 -0500

    Fixed possible division by 0

--- springconnection.cpp
+++ springconnection.cpp
@@ -21,6 +21,10 @@ void SpringConnection::addForce() {
     (*object2)->getModelSpacePointInWorldCoordinates(object2ConnectionPosition,end2pos);
     q_vec_subtract(difference,end2pos,end1pos);
     length = q_vec_magnitude(difference);
+    if (length < Q_EPSILON) {
+        return; // even if we don't have rest length 0, what direction do we push in?
+                // just return...
+    }
     q_vec_normalize(difference,difference);
     displacement = restLength - length;
     q_vec_scale(f2,displacement*stiffness,difference);
