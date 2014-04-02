7760f8ad39d3ede222dd9649d8620aa6d04423f6
lluhi <lluhi@live.unc.edu>
2014-04-02 19:11:56 -0400

    Added color functions test

--- Input/test/TestGlobalControls.cxx
+++ Input/test/TestGlobalControls.cxx
@@ -54,7 +54,8 @@ inline int testPositionOfObject(SketchProject *project, q_vec_type expected)
     obj->getPosition(actual);
     if (!q_vec_equals(expected,actual))
     {
-        std::cout << "Wrong position after undo." << std::endl;
+        std::cout << "Error at " << __FILE__ << ":" << __LINE__ <<
+              "  Wrong position after undo." << std::endl;
         return 1;
     }
     return 0;
