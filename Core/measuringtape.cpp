540b1d0f960e7c80652aac8f2a7f7c4de4dc97e2
Patrick Hahn <phahn@live.unc.edu>
2014-05-16 13:11:56 -0400

    Adding new files for measuring tape.

---
+++ Core/measuringtape.cpp
@@ -0,0 +1,24 @@
+#include "measuringtape.h"
+
+MeasuringTape::MeasuringTape(SketchObject* o1, SketchObject* o2, const q_vec_type o1Pos,
+              const q_vec_type o2Pos, bool display)
+	:
+	Connector(o1, o2, o1Pos, o2Pos, TAPE_ALPHA_VALUE,
+                TAPE_DISPLAY_RADIUS, display),
+				lengthActor(vtkSmartPointer< vtkTextActor3D >::New())
+{
+	QString lengthstr = QString::number(getLength());
+	lengthActor->SetInput(lengthstr.toStdString().c_str());
+	q_vec_type midpt;
+	getMidpoint(midpt);
+	lengthActor->SetPosition(midpt);
+}
+
+void MeasuringTape::getMidpoint(q_vec_type out) {
+	q_vec_type pos1, pos2;
+	getObject1ConnectionPosition(pos1);
+	getObject2ConnectionPosition(pos2);
+	out[0] = (pos1[0] + pos2[0]) / 2;
+	out[1] = (pos1[1] + pos2[1]) / 2;
+	out[2] = (pos1[2] + pos2[2]) / 2;
+}
---
+++ Core/measuringtape.h
@@ -0,0 +1,35 @@
+#ifndef MEASURINGTAPE_H
+#define MEASURNIGTAPE_H
+
+#include <quat.h>
+#include "connector.h"
+
+#include <vtkTextActor3D.h>
+#include <vtkSmartPointer.h>
+class vtkTextActor3D;
+
+// the default values of alpha and radius for a MeasuringTape
+static const double TAPE_ALPHA_VALUE = 0.5;
+static const double TAPE_DISPLAY_RADIUS = 2.0;
+
+/*
+ * This class is a type of connector that has a length and will be displayed as a line
+ * along with its length and tick marks to act as a measuring tape, so it can be used to
+ * measure distances and determine the scale of objects
+*/
+
+class MeasuringTape : public Connector
+{
+public:
+	MeasuringTape(SketchObject* o1, SketchObject* o2, const q_vec_type o1Pos,
+              const q_vec_type o2Pos, bool display = true);
+    virtual ~MeasuringTape();
+
+	vtkTextActor3D* getLengthActor() const { return lengthActor; }
+	
+	void getMidpoint(q_vec_type out);
+private:
+	vtkSmartPointer< vtkTextActor3D > lengthActor;
+};
+
+#endif
